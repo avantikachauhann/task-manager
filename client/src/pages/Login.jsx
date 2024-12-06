c4400105e30e377d67803ef7e80f117815c1572c
Avantika Chauhan <avantikachauhn@gmail.com>
2024-12-06 15:06:45 +0530

    Working on dashboard page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   useEffect(() => {
     user && navigate("/dashboard");
   }, [user]);
-
+  
   return (
     <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
       <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
