1f120dd0f5bb465a19e6b5a4ce94aded711d3d70
Avantika Chauhan <avantikachauhn@gmail.com>
2025-01-05 22:43:12 +0530

    setting up deployment

--- client/src/redux/slices/api/authApiSlice.js
+++ client/src/redux/slices/api/authApiSlice.js
@@ -6,7 +6,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${AUTH_URL}/login`, // Use backticks instead of double quotes
+        url: `${AUTH_URL}/login`, 
         method: "POST",
         body: data,
         credentials: "include", // Optional: Include credentials (cookies, etc.) if required
